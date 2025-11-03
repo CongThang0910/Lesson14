c62d8dcf6bd447ad899ae012582ba7569c6ddc46
system <congthang091004@gmail.com>
2025-11-03 13:54:21 +0700

    update code

--- 14.3.cpp
+++ 14.3.cpp
@@ -1,76 +1,135 @@
 #include "stm32f10x.h"
-#include "FreeRTOS.h"
-#include "task.h"
-#include "event_groups.h"
+#include "stm32f10x_rcc.h"
+#include "stm32f10x_gpio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x_pwr.h"
+#include "stm32f10x_rtc.h"
+#include "misc.h"
 
-#define LED1_PIN GPIO_Pin_0
-#define LED2_PIN GPIO_Pin_1
-#define LED3_PIN GPIO_Pin_10
-#define LED_PORT GPIOB
+void UART1_Init(void);
+void UART1_SendString(char *str);
+void RTC_Config(void);
+void Enter_Standby(void);
 
-#define BIT_TASK1 (1 << 0)
-#define BIT_TASK2 (1 << 1)
-#define BIT_TASK3 (1 << 2)
-
-EventGroupHandle_t xEventGroup;
-
-void GPIO_Config(void) {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-    GPIO_InitTypeDef gpio;
-    gpio.GPIO_Mode = GPIO_Mode_Out_PP;
-    gpio.GPIO_Speed = GPIO_Speed_2MHz;
-    gpio.GPIO_Pin = LED1_PIN | LED2_PIN | LED3_PIN;
-    GPIO_Init(LED_PORT, &gpio);
-}
-
-void Task1(void *pvParameters) {
-    for (;;) {
-        xEventGroupWaitBits(xEventGroup, BIT_TASK1, pdTRUE, pdFALSE, portMAX_DELAY);
-        GPIO_SetBits(LED_PORT, LED1_PIN);
-        vTaskDelay(500 / portTICK_RATE_MS);
-        GPIO_ResetBits(LED_PORT, LED1_PIN);
+int main(void)
+{
+    /* N?u v?a thoát kh?i ch? d? Standby */
+    if (PWR_GetFlagStatus(PWR_FLAG_SB) != RESET)
+    {
+        PWR_ClearFlag(PWR_FLAG_SB);      // Xóa c? Standby
+        UART1_Init();
+        UART1_SendString("Wake up! System restarted\r\n");
     }
-}
-
-void Task2(void *pvParameters) {
-    for (;;) {
-        xEventGroupWaitBits(xEventGroup, BIT_TASK2, pdTRUE, pdFALSE, portMAX_DELAY);
-        GPIO_SetBits(LED_PORT, LED2_PIN);
-        vTaskDelay(500 / portTICK_RATE_MS);
-        GPIO_ResetBits(LED_PORT, LED2_PIN);
+    else
+    {
+        UART1_Init();
+        UART1_SendString("Normal start\r\n");
     }
-}
 
-void Task3(void *pvParameters) {
-    for (;;) {
-        xEventGroupWaitBits(xEventGroup, BIT_TASK3, pdTRUE, pdFALSE, portMAX_DELAY);
-        GPIO_SetBits(LED_PORT, LED3_PIN);
-        vTaskDelay(500 / portTICK_RATE_MS);
-        GPIO_ResetBits(LED_PORT, LED3_PIN);
-    }
-}
+    RTC_Config();
 
-void Task_Control(void *pvParameters) {
-    for (;;) {
-        xEventGroupSetBits(xEventGroup, BIT_TASK1);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xEventGroupSetBits(xEventGroup, BIT_TASK2);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xEventGroupSetBits(xEventGroup, BIT_TASK3);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        xEventGroupSetBits(xEventGroup, BIT_TASK1 | BIT_TASK2 | BIT_TASK3);
-        vTaskDelay(2000 / portTICK_RATE_MS);
-    }
-}
+    UART1_SendString("Entering Standby...\r\n");
+    for (volatile int i = 0; i < 500000; i++); // Delay ng?n d? UART g?i xong
 
-int main(void) {
-    SystemInit();
-    GPIO_Config();
-    xEventGroup = xEventGroupCreate();
-    xTaskCreate(Task_Control, "Ctrl", 128, NULL, 2, NULL);
-    xTaskCreate(Task1, "T1", 128, NULL, 1, NULL);
-    xTaskCreate(Task2, "T2", 128, NULL, 1, NULL);
-    xTaskCreate(Task3, "T3", 128, NULL, 1, NULL);
-    vTaskStartScheduler();
+    Enter_Standby();
+
     while (1);
+}
+
+void UART1_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+    USART_InitTypeDef USART_InitStruct;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_USART1, ENABLE);
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_9; // TX
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10; // RX
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    USART_InitStruct.USART_BaudRate = 9600;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
+    USART_Init(USART1, &USART_InitStruct);
+
+    USART_Cmd(USART1, ENABLE);
+}
+
+void UART1_SendString(char *str)
+{
+    while (*str)
+    {
+        USART_SendData(USART1, *str++);
+        while (USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+    }
+}
+
+void RTC_Config(void)
+{
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR | RCC_APB1Periph_BKP, ENABLE);
+    PWR_BackupAccessCmd(ENABLE);
+
+    if (BKP_ReadBackupRegister(BKP_DR1) != 0xA5A5)
+    {
+        RCC_LSEConfig(RCC_LSE_ON);
+        while (RCC_GetFlagStatus(RCC_FLAG_LSERDY) == RESET);
+
+        RCC_RTCCLKConfig(RCC_RTCCLKSource_LSE);
+        RCC_RTCCLKCmd(ENABLE);
+
+        RTC_WaitForSynchro();
+        RTC_WaitForLastTask();
+
+        RTC_SetPrescaler(32767);  // 1Hz tick
+        RTC_WaitForLastTask();
+
+        RTC_SetAlarm(RTC_GetCounter() + 5); // 5 giây
+        RTC_WaitForLastTask();
+
+        BKP_WriteBackupRegister(BKP_DR1, 0xA5A5);
+    }
+    else
+    {
+        RTC_WaitForSynchro();
+        RTC_WaitForLastTask();
+        RTC_SetAlarm(RTC_GetCounter() + 5);
+        RTC_WaitForLastTask();
+    }
+
+    RTC_ITConfig(RTC_IT_ALR, ENABLE);
+    RTC_WaitForLastTask();
+
+    NVIC_InitTypeDef NVIC_InitStruct;
+    NVIC_InitStruct.NVIC_IRQChannel = RTC_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0;
+NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+void RTC_IRQHandler(void)
+{
+    if (RTC_GetITStatus(RTC_IT_ALR) != RESET)
+    {
+        RTC_ClearITPendingBit(RTC_IT_ALR);
+        RTC_WaitForLastTask();
+
+        RTC_SetAlarm(RTC_GetCounter() + 5);
+        RTC_WaitForLastTask();
+    }
+}
+
+void Enter_Standby(void)
+{
+    PWR_ClearFlag(PWR_FLAG_WU);
+
+    PWR_EnterSTANDBYMode();
 }
